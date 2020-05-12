b94956c07f4e290a69ea40b9079e548779cb1f0f
Grant Holley <gvholley@gmail.com>
2020-05-12 11:16:21 +0900

    updated files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+  # whatever you do here, every controller has access to
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # CRUD
+  # create, read, update, destroy
+  # verb, '/path', 'to: controller#action'
+  # Read all
+  get '/restaurants', to: 'restaurants#index', as: :restaurants
+  # Read one
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # Create
+  # a simple get request for a form page
+  get 'resaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # where do we want to send the form?
+  post '/restaurants', to: 'restaurants#create'
+  # Update
+  # a simple get request for a form page
+  get '/restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  patch '/restaurants/:id', to: 'restaurants#update'
+
+  # Destroy
+  delete 'restaurants/:id', to: 'restaurants#destroy'
 end
